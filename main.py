f10c98c2f643957dc1ef510dc6695ca6f7191aaf
3dbtori <107370697+3dbtori@users.noreply.github.com>
2024-08-31 07:53:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from PIL import Image
 st.write('Display Image')
 img1 = Image.open('octocat1.png')
 img2 = Image.open('octocat2.jpeg')
-st.image(img1, caption='octocat1', use_column_width=True)
 
 # ボタンで画像表示、非表示
 if st.checkbox('show octocat2?'):
     st.image(img2, caption='octocat2', use_column_width=True)
+else:
+    st.image(img1, caption='octocat1', use_column_width=True)
+
+
